cd82771c7b1213abba4ca7ef5b18f2db3f057288
panchenglong.jacky <panchenglong.jacky@bytedance.com>
2026-08-31 15:24:57 +0800

    docs: restore bill api doc links
    Co-Authored-By: Aime <aime@bytedance.com>
    Change-Id: Ic55b705cee8ed15a0c8869dc04b4ec7bf6e96eb5

--- src/main/java/com/douyinpay/api/bill/ApiBillService.java
+++ src/main/java/com/douyinpay/api/bill/ApiBillService.java
@@ -14,6 +14,17 @@ import com.douyinpay.component.http.QueryParameter;
 import com.douyinpay.define.DomainName;
 import com.douyinpay.util.StringUtil;
 
+/**
+ * 账单服务。
+ *
+ * 官方文档：
+ * - 申请交易账单：GET /v1/bill/billapply
+ * https://pay.douyinpay.com/wiki/639fd48f17c2f3021d237f61/667e6bc444a74902ead102ad
+ * - 申请资金账单：GET /v1/bill/fundflowbill
+ * https://pay.douyinpay.com/wiki/639fd48f17c2f3021d237f61/667e69daa998e00305dcec48
+ * - 申请分账账单：GET /v1/bill/splitbill
+ * https://pay.douyinpay.com/wiki/639fd48f17c2f3021d237f61/68355a92994b190515a2af86
+ */
 public class ApiBillService {
 
     /**
